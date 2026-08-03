7015ce46746c2df7aa2f6a7b5408c5a77591f10a
whitebelyash <whbexiumwork@gmail.com>
2026-08-03 21:50:01 +0400

    Android: fix NULL dereference in event poller
    Fixes Ixeris

--- src/android_window.c
+++ src/android_window.c
@@ -813,8 +813,7 @@ void _glfwPollEventsAndroid(void)
 {
     process_flag_bits();
     _input_queue_dequeue(&input_queue, android_dequeue_event);
-
-    if((surfaceOwner->android.mode == GLFW_ANDROID_WINDOW_MODE_SURFACE || ownedByVulkan) && surfaceUpdated) {
+    if(((surfaceOwner && surfaceOwner->android.mode == GLFW_ANDROID_WINDOW_MODE_SURFACE) || ownedByVulkan) && surfaceUpdated) {
         updateNativeWindowDimensions(surfaceOwner);
     }
 }
